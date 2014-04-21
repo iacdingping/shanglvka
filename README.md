a4f61d379e145db1128214474df4f49bcb901dc0
iacdp <iacdingping@gmail.com>
2014-04-21 23:16:30 +0800

    jfinal 示例代码

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ shanglvka
 =========
 
 商旅卡
+
+shanglvka-core 主要功能模块
+shanglvka-wap 手机端wap web应用
+shanglvka-manager 后台管理系统 
---
+++ shanglvka-manager/src/main/java/com/demo/common/CommonController.java
@@ -0,0 +1,13 @@
+package com.demo.common;
+
+import com.jfinal.core.Controller;
+
+/**
+ * CommonController
+ */
+public class CommonController extends Controller {
+	
+	public void index() {
+		render("/common/index.html");
+	}
+}
