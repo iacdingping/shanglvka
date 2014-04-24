c80599766ff896aa19bc08700f364e43fb21de95
iacdp <iacdingping@gmail.com>
2014-04-24 11:22:00 +0800

    spring-boot profiles使用

--- shanglvka-core/src/test/java/com/iac/slk/model/platform/PlatformRepositoryTests.java
+++ shanglvka-core/src/test/java/com/iac/slk/model/platform/PlatformRepositoryTests.java
@@ -23,6 +23,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
