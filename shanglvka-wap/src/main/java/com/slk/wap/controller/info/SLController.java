842cad4f38fe7ba339892df2549748f5f7d0c009
sycz925 <yu_bo925@163.com>
2014-08-23 20:14:06 +0800

    初始导入。

--- shanglvka-wap/src/main/java/com/slk/wap/controller/info/SLController.java
+++ shanglvka-wap/src/main/java/com/slk/wap/controller/info/SLController.java
@@ -1,7 +1,13 @@
 package com.slk.wap.controller.info;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.slk.hibernate.core.entity.MerchantMap;
+import com.slk.hibernate.core.service.MerchantMapService;
 
 /**
  * 商旅
@@ -9,21 +15,29 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/sl")
 public class SLController {
+	@Autowired
+	private MerchantMapService merchantMapService;
+
 	/**
 	 * 列表
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/list")
-	public String list(){
+	public String list(Model model) {
+		model.addAttribute("merchantMaps", merchantMapService.list());
 		return "/info/sl/list";
 	}
-	
+
 	/**
 	 * 详情
+	 * 
 	 * @return
 	 */
 	@RequestMapping("/detail/{slID}")
-	public String detail(){
+	public String detail(Model model,@PathVariable Long slID) {
+		MerchantMap merchantMap = merchantMapService.get(slID);
+		model.addAttribute("merchantMap", merchantMap);
 		return "/info/sl/detail";
 	}
 }
